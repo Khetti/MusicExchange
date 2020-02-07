e3bfc108729397c7b7926e1877ad4c555a93f7e9
Greg Shanks <greg.shanks@gmail.com>
2020-02-07 12:25:15 +0000

    Initialised Instrument superclass; Guitar subclass testing next

--- src/main/java/Guitar.java
+++ src/main/java/Guitar.java
@@ -1,4 +1,4 @@
-public class Guitar {
+public class Guitar extends Instrument {
 
 
 
--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -1,5 +1,15 @@
 public abstract class Instrument {
 
+    private String brand;
+    private String colour;
+    private double buyPrice;
+    private double sellPrice;
 
+    public Instrument(String brand, String colour, double buyPrice, double sellPrice) {
+        this.brand = brand;
+        this.colour = colour;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
 
 }
